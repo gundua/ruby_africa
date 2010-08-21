30635de471d6030903409b62d23299803874facd
Hendrik Louw <hflouw@gmail.com>
2010-08-21 16:06:12 +0200

    rails 3 rc setup with cucumber

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyAfrica::Application.config.secret_token = 'c131515ad7230eb192c55d64b9b51ebc91e5efdd9d3e0f7a244cc0919511b5f4fdd5810fd8108af34691eb6b98dc717ca0bf4b4d37dfa9b529e2c99e5f52b7e5'
