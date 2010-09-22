9c62b047f0487c3623d61b3abd84b59cf1a72740
Peter Garbers <peter.garbers@gmail.com>
2010-09-22 14:28:47 +0200

    Simple changes to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User
   include Mongoid::Document
-
+  field :username, :type => String
+  field :first_name, :type => String
+  field :last_name, :type => String
+  field :dob, :type => Date
+  field :twitter, :type => String
   devise  :database_authenticatable, :lockable, :rememberable, :registerable, :trackable, :recoverable
 end
